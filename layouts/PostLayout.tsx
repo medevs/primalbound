8920d15cc7510e2db8e365436d5fe69531e5ac7f
Ahmed Oublihi <a.oublihi@ephilos.lokal>
2023-08-31 22:22:21 +0200

    read time

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -9,6 +9,8 @@ import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+import { HiOutlineClock } from 'react-icons/hi'
+import readingTime from 'reading-time'
 
 const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
 const discussUrl = (path) =>
@@ -30,7 +32,7 @@ interface LayoutProps {
 }
 
 export default function PostLayout({ content, authorDetails, next, prev, children }: LayoutProps) {
-  const { filePath, path, slug, date, title, tags } = content
+  const { readingTime, filePath, path, slug, date, title, tags } = content
   const basePath = path.split('/')[0]
 
   return (
@@ -40,18 +42,17 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
             <div className="space-y-1 text-center">
-              <dl className="space-y-10">
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </dd>
-                </div>
-              </dl>
               <div>
                 <PageTitle>{title}</PageTitle>
+              </div>
+              <div className="flex justify-center gap-5 py-4">
+                <time dateTime={date}>
+                  {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+                </time>
+                <span className="flex items-center gap-1.5">
+                  <HiOutlineClock className="h-5 w-5" />
+                  {readingTime.text}
+                </span>
               </div>
             </div>
           </header>
